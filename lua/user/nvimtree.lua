65ab2093f9a65c9b3caf7bf4e0903be4eb954094
Gaurav Kashyap <gaurkash@flow.local>
2025-02-28 09:18:02 -0800

    Nvim-tree cwd options

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -3,10 +3,21 @@ local M = {
 }
 
 M.config = function()
-  local tree = require "nvim-tree"
-  local wk = require "which-key"
+  local tree = require("nvim-tree")
+  local wk = require("which-key")
 
-  local opts = {}
+  local opts = {
+    respect_buf_cwd = true,
+    sync_root_with_cwd = true,
+    update_focused_file = {
+      enable = true,
+      update_root = {
+        enable = true,
+        ignore_list = {},
+      },
+      exclude = false,
+    },
+  }
 
   tree.setup(opts)
 
